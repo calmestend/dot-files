5762fca6466be2f925e3fb81b467cdba89e2b148
Barac Fabregat <barac.fabregat.gallegos@tuta.com>
2025-04-12 22:49:28 -0600

    add dmenu config

---
+++ dmenu/config.def.h
@@ -0,0 +1,22 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {"Iosevka Nerd Font:size=10"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+    /*     fg         bg       */
+    [SchemeNorm] = {"#ffffff", "#000000"},
+    [SchemeSel] = {"#000000", "#ffffff"},
+    [SchemeOut] = {"#000000", "#ffffff"},
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
